6d63fb9953fffbc278c02a575c36565e6e2c49da
Mateja <145758273+mateja404@users.noreply.github.com>
2025-06-30 14:25:02 +0200

    Update page.tsx

---
+++ app/projects/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import HomePage from "./components/Home";
+
+export default function Projects() {
+  return (
+    <div className="relative flex min-h-screen w-full bg-white dark:bg-emerald-950/60">
+      <div className="absolute inset-0 
+        [background-size:12px_12px]
+        [background-image:linear-gradient(to_right,rgba(16,185,129,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(16,185,129,0.08)_1px,transparent_1px)] 
+        dark:[background-image:linear-gradient(to_right,rgba(5,150,105,0.12)_1px,transparent_1px),linear-gradient(to_bottom,rgba(5,150,105,0.12)_1px,transparent_1px)]">
+      </div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_70%,black)] dark:bg-black"></div>
+      <HomePage/>
+    </div>
+  );
+}
